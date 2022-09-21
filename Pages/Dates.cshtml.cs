ded358be2bc453b6d4d85519a5eb919b0090fa17
dianamaebonayog <dbonayog@csmbataan.ph.education>
2022-09-21 16:16:22 +0800

    Prelim Activity 1

---
+++ Pages/Dates.cshtml.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BonayogDianaMae.DateCalculator.Pages
+{
+    public class DatesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+
+
+    }
+}
+
